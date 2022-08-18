f525c289e0dc1822704fa559c82ae819529d1bd5
Eric Gilbert <e.gilbert.1@elsevier.com>
2022-08-18 18:13:12 -0400

    added helper functions

---
+++ src/funcs/utils.py
@@ -0,0 +1,22 @@
+def inOutkeys_to_lists(df):
+    first_ids = []
+    second_ids = []
+    df['inOutkey'] = df['inOutkey'].apply(lambda x: x.replace('[', ''))
+    df['inOutkey'] = df['inOutkey'].apply(lambda x: x.replace(']', ''))
+
+    inOutkey_list = list(df['inOutkey'])
+    for item in inOutkey_list:
+        x = item.split(',')
+        first_ids.append(x[0].strip())
+        second_ids.append(x[1].strip())
+
+    return first_ids, second_ids
+
+
+def convert_object_to_category(df):
+    df_obj = df.select_dtypes(['object'])
+
+    for col in list(df_obj.columns):
+        df[col] = df[col].apply(lambda x: x.strip())
+        df[col] = df[col].astype('category')
+    return df
