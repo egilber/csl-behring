b86b88f86b46f90db5e1ea19270c7fb317be951a
egilber <e.gilbert.1@elsevier.com>
2024-06-14 17:39:02 -0400

    initial commit

---
+++ src/scripts/preprocess_resnet.py
@@ -0,0 +1,173 @@
+import pickle
+import pandas as pd
+from typing import Any, Optional, List, Tuple, List, NoReturn
+from configs.paths import DataPaths
+
+
+def inOutkeys_to_lists(df: pd.DataFrame) -> Tuple[List[str], List[str]]:
+    """
+    Splits 'inOutkey' column of a dataframe into two lists.
+
+    Args:
+        df (pd.DataFrame): Input dataframe which contains 'inOutkey' column.
+
+    Returns:
+        Tuple[List[str], List[str]]: Two lists representing the first and second IDs.
+    """
+
+    first_ids = []
+    second_ids = []
+    df['inOutkey'] = df['inOutkey'].apply(lambda x: x.replace('[', '').replace(']', ''))
+    for item in df['inOutkey'].tolist():
+        x = item.split(',')
+        first_ids.append(x[0].strip())
+        second_ids.append(x[1].strip())
+
+    return first_ids, second_ids
+
+
+def convert_object_to_category(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts all object type columns in a dataframe into category type.
+
+    Args:
+        df (pd.DataFrame): Input dataframe.
+
+    Returns:
+        pd.DataFrame: Dataframe with object type columns converted to category type.
+    """
+
+    df_obj = df.select_dtypes(['object'])
+    for col in list(df_obj.columns):
+        df[col] = df[col].apply(lambda x: str(x).strip())
+        df[col] = df[col].astype('category')
+    return df
+
+
+def create_header_file(cols: List[str], header_path) -> None:
+    """
+    Creates a header file.
+
+    Args:
+        cols (List[str]): List of column names.
+        header_path (str): Path where the header file will be saved.
+    """
+
+    df_headers = pd.DataFrame(columns=cols)
+    df_headers.to_csv(header_path, sep='|', index=False)
+
+
+class DataProcessor:
+    def __init__(self) -> None:
+        """
+        Initialize the DataProcessor object, setting up paths.
+        """
+
+        self.paths = DataPaths()
+
+    def process_directional_rels(self) -> None:
+        """
+        Process directional relationships from raw data and save it as a pickle file.
+        """
+
+        output_path = self.paths.get_full_path(self.paths.directional_rels_procd)
+        df = pd.read_csv(self.paths.get_full_path(self.paths.directional_rels), sep='|', header=None, encoding='utf-8')
+        df.columns = ['msrc_id', ':START_ID', 'type:TYPE', 'effect', 'mechanism', 'ref_count:int', ':END_ID',
+                      'id2', 'biomarkertype', 'celllinename', 'celltype', 'changetype', 'organ', 'organism',
+                      'quantitativetype', 'tissue', 'nct_id', 'phase']
+        df = df.drop(columns=['id2'])
+        df['phase'] = df['phase'].fillna('None')
+        df = convert_object_to_category(df)
+        df['ref_count:int'] = df['ref_count:int'].astype('int16')
+        with open(output_path, 'wb') as file:
+            pickle.dump(df, file)
+
+    def process_bi_directional_rels(self) -> None:
+        """
+        Process bidirectional relationships from raw data and save it as a pickle file.
+        """
+
+        output_path = self.paths.get_full_path(self.paths.bidirectional_rels_procd)
+
+        df = pd.read_csv(self.paths.get_full_path(self.paths.bidirectional_rels), sep='|', header=None,
+                         encoding='utf-8')
+        df.columns = ['msrc_id', ':START_ID', 'inOutkey', 'type:TYPE', 'relationship', 'effect', 'mechanism',
+                      'ref_count:int', ':END_ID', 'id2', 'biomarkertype', 'celllinename', 'celltype',
+                      'changetype', 'organ', 'organism', 'quantitativetype', 'tissue']
+        first_ids, second_ids = inOutkeys_to_lists(df)
+        df.drop(columns=['inOutkey', 'id2', 'relationship'], inplace=True)
+        df[':START_ID'] = first_ids
+        df[':END_ID'] = second_ids
+        df[':START_ID'] = df[':START_ID'].astype('int64')
+        df[':END_ID'] = df[':END_ID'].astype('int64')
+        df['ref_count:int'] = df['ref_count:int'].astype('int16')
+        df = convert_object_to_category(df)
+        with open(output_path, 'wb') as file:
+            pickle.dump(df, file)
+
+    def process_attribute_rels(self) -> None:
+        """
+        Process attribute relationships from raw data and save it as a pickle file.
+        """
+
+        output_path = self.paths.get_full_path(self.paths.attribute_rels_procd)
+        df = pd.read_csv(self.paths.get_full_path(self.paths.attribute_rels), sep='|', header=None, encoding='utf-8',
+                         low_memory=False)
+        df.columns = ['msrc_id', ':START_ID', 'id2', 'type:TYPE', ':END_ID']
+        df = df.drop(columns=['id2'])
+        df = df.dropna(how='any')
+        df.reset_index(drop=True, inplace=True)
+        for col in list(df.columns):
+            df[col] = df[col].apply(lambda x: str(x).strip())
+        df[':START_ID'] = pd.to_numeric(df[':START_ID'], errors='coerce').astype('int64')
+        df[':END_ID'] = pd.to_numeric(df[':END_ID'], errors='coerce').astype('int64')
+        df['type:TYPE'] = df['type:TYPE'].astype('category')
+        with open(output_path, 'wb') as file:
+            pickle.dump(df, file)
+
+    def concat_relationship_files(self) -> None:
+        """
+       Concatenate processed relationship dataframes and save to a file.
+       """
+        output_path = self.paths.get_full_path(self.paths.concat_relationships_procd)
+
+        df_directional = pd.read_pickle(self.paths.get_full_path(self.paths.directional_rels_procd))
+        df_biDirectional = pd.read_pickle(self.paths.get_full_path(self.paths.bidirectional_rels_procd))
+        df_att = pd.read_pickle(self.paths.get_full_path(self.paths.attribute_rels_procd))
+
+        df_concat = pd.concat([df_directional, df_biDirectional, df_att], ignore_index=True)
+        df_concat['ref_count:int'].fillna(0, inplace=True)
+        for col in df_concat.select_dtypes(include=['category']).columns:
+            df_concat[col] = df_concat[col].astype('object')
+        df_concat.fillna('None', inplace=True)
+        df_concat = df_concat.replace('nan', 'None')
+        df_concat = df_concat.replace('None', '_')
+
+        df_concat['ref_count:int'] = df_concat['ref_count:int'].astype('int16')
+        df_concat['msrc_id'] = df_concat['msrc_id'].astype('int64')
+        df_concat = convert_object_to_category(df_concat.drop_duplicates().reset_index(drop=True))
+
+        df_concat['type:TYPE'] = df_concat['type:TYPE'].apply(lambda x: x.upper())
+        cols = list(df_directional.columns)
+        df_concat[cols].to_csv(output_path, sep='|', index=False, header=True)
+
+        header_path = self.paths.get_full_path(self.paths.relationships_header)
+        create_header_file(cols, header_path)
+
+    def process_node_file(self) -> None:
+        """
+        Process node data from raw data and save it as a CSV file.
+        """
+        output_path = self.paths.get_full_path(self.paths.nodes_procd)
+        df = pd.read_csv(self.paths.get_full_path(self.paths.nodes_raw), sep='|', header=None, encoding='utf-8',
+                         low_memory=False)
+        df.columns = [':ID', 'name', ':LABEL']
+        df = df.applymap(lambda x: str(x).strip())
+        df[':LABEL'] = df[':LABEL'].apply(lambda x: x.upper())
+        df[':ID'] = df[':ID'].astype('int64')
+        df['name'] = df['name'].replace([';;', ';'], ':', regex=True)
+
+        df.to_csv(output_path, sep='|', index=False, header=True)
+        cols = list(df.columns)
+        header_path = self.paths.get_full_path(self.paths.nodes_header)
+        create_header_file(cols, header_path)
---
+++ src/scripts/resnet_datasets.py
@@ -0,0 +1,112 @@
+import psycopg2 as ps2
+import os
+import csv
+from dotenv import load_dotenv
+load_dotenv()
+
+database_name = 'resnet18'
+
+from configs.database import SQLDBCreds
+from configs.paths import DataPaths
+
+directional_sql_query = f"SELECT \
+control.id, inkey[1], controltype,  string_agg(distinct(effect), ', '), string_agg(distinct(mechanism), ', '), \
+num_refs, outkey[1] , reference.id, string_agg(distinct(biomarkertype), ', ') ,\
+string_agg(distinct(celllinename), ', '), string_agg(distinct(celltype), ', '), string_agg(distinct(changetype), ', '),\
+string_agg(distinct(organ), ', '), string_agg(distinct(organism), ', '), string_agg(distinct(quantitativetype), ', '), \
+string_agg(distinct(tissue), ', '), string_agg(distinct(nct_id), ', '),  string_agg(distinct(phase), ', ') \
+FROM {database_name}.control, {database_name}.reference \
+WHERE control.id = reference.id and inkey[1] is not null and outkey[1] is not null \
+GROUP BY control.id, inkey[1], controltype, num_refs, outkey[1], reference.id"
+
+bi_directional_sql_query = f"SELECT \
+control.id, inkey[1], inoutkey, controltype, relationship, string_agg(distinct(effect), ', '), \
+string_agg(distinct(mechanism), ', '), num_refs, outkey[1], reference.id, string_agg(distinct(biomarkertype), ', '), \
+string_agg(distinct(celllinename), ', '), string_agg(distinct(celltype), ', '), string_agg(distinct(changetype), ', '),\
+string_agg(distinct(organ), ', '), string_agg(distinct(organism), ', '), string_agg(distinct(quantitativetype), ', '), \
+string_agg(distinct(tissue), ', ') \
+FROM {database_name}.control, {database_name}.reference \
+WHERE control.id = reference.id and inkey[1] is null and outkey[1] is null \
+GROUP BY control.id, controltype, reference.id"
+
+attributes_sql_query = f"SELECT  \
+id, inkey[1], attributes, relationship, outkey[1] from {database_name}.control \
+WHERE (control.id = control.attributes and inkey[1] is not null and outkey[1] is not null)"
+
+node_sql_query = 'select id, name, nodetype from resnet18.node where id is not null and name is not null and nodetype \
+ is not null'
+
+
+class CreateDatasets:
+    def __init__(self) -> None:
+        """
+        Initialize the CreateDatasets object, setting up paths.
+        """
+        self.paths = DataPaths()
+
+    def get_sqldb_creds(self) -> SQLDBCreds:
+        db_creds = SQLDBCreds(
+            db_name=os.getenv('DB_NAME'),
+            db_user=os.getenv('DB_USER'),
+            db_host=os.getenv('DB_HOST_IP'),
+            db_pwd=os.getenv('DB_PWD')
+        )
+        return db_creds
+
+    def execute_sql_query(self, sql_query: str, output_path) -> None:
+        """
+        Execute the given SQL query and save the results to a file.
+
+        Args:
+            sql_query (str): The SQL query to execute.
+            output_path (str): The path to the output file.
+        """
+        creds = self.get_sqldb_creds()
+        conn = ps2.connect(
+            dbname=creds.db_name,
+            user=creds.db_user,
+            password=creds.db_pwd,
+            host=creds.db_host
+        )
+        try:
+            with conn.cursor() as cur:
+                with open(output_path, 'w', encoding="utf-8") as f:
+                    cur.execute(sql_query)
+                    csv_writer = csv.writer(f, delimiter='|')
+                    for record in cur.fetchall():
+                        csv_writer.writerow(record)
+        except ps2.DatabaseError as e:
+            raise ps2.DatabaseError(f"Database operation failed: {e}") from e
+        except IOError as e:
+            raise IOError(f"Failed to write to file at {output_path}: {e}") from e
+        finally:
+            if conn:
+                conn.close()
+
+    def create_directional_ds(self) -> None:
+        """
+        Create a dataset for directional relationships and save it to a file.
+        """
+        output_path = self.paths.get_full_path(self.paths.directional_rels)
+        self.execute_sql_query(directional_sql_query, output_path)
+
+    def create_bi_directional_ds(self) -> None:
+        """
+        Create a dataset for bidirectional relationships and save it to a file.
+        """
+        output_path = self.paths.get_full_path(self.paths.bidirectional_rels)
+        self.execute_sql_query(bi_directional_sql_query, output_path)
+
+    def create_attributes_ds(self) -> None:
+        """
+        Create a dataset for attribute relationships and save it to a CSV file.
+        """
+        output_path = self.paths.get_full_path(self.paths.attribute_rels)
+        self.execute_sql_query(attributes_sql_query, output_path)
+
+    def create_nodes_ds(self) -> None:
+        """
+        Create a dataset for nodes and save it to a CSV file.
+        """
+        output_path = self.paths.get_full_path(self.paths.nodes_raw)
+        self.execute_sql_query(node_sql_query, output_path)
